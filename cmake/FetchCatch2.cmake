829034b5bc77a3df871fb0a9b58e0b7c55c14ef5
emmett <emmettzhang2020@outlook.com>
2025-01-12 04:42:06 +0000

    try to add git progress

--- cmake/FetchCatch2.cmake
+++ cmake/FetchCatch2.cmake
@@ -5,6 +5,7 @@ FetchContent_Declare(
   Catch2
   GIT_REPOSITORY https://github.com/catchorg/Catch2.git
   GIT_TAG v3.6.0
+  GIT_PROGRESS TRUE
 )
 message(STATUS "Downloading Catch2")
 FetchContent_MakeAvailable(Catch2)
--- cmake/FetchMagicEnum.cmake
+++ cmake/FetchMagicEnum.cmake
@@ -4,6 +4,7 @@ FetchContent_Declare(
   magic_enum
   GIT_REPOSITORY https://github.com/Neargye/magic_enum.git
   GIT_TAG v0.9.6
+  GIT_PROGRESS TRUE
 )
 
 FetchContent_MakeAvailable(magic_enum)
