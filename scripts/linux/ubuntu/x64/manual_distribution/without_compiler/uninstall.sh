f9718ba260b1e693659cb3be84135bef1c8a6360
Emmett <emmettzhang2020@outlook.com>
2025-01-25 00:39:53 +0800

    release 0.0.1 (#12)

--- scripts/linux/ubuntu/x64/manual_distribution/without_compiler/uninstall.sh
+++ scripts/linux/ubuntu/x64/manual_distribution/without_compiler/uninstall.sh
@@ -19,16 +19,16 @@ set -euo pipefail
 
 BINARY_NAME="cpp-lint-action"
 LIB_INSTALL_PATH="$HOME/.local/lib/${BINARY_NAME}/"
-BIN_INSTALL_PATH="$HOME/.local/bin/"
+BIN_INSTALL_PATH="$HOME/.local/bin"
 
 # Refresh libraries
 if [[ -d "${LIB_INSTALL_PATH}" ]]; then
   echo "Remove old ${BINARY_NAME} libraries"
   sudo rm -rf ${LIB_INSTALL_PATH}
 fi
-if [[ -d "${BIN_INSTALL_PATH}" ]]; then
+if [[ -f "${BIN_INSTALL_PATH}/${BINARY_NAME}" ]]; then
   echo "Remove old ${BINARY_NAME} binaries"
-  sudo rm -rf ${BIN_INSTALL_PATH}
+  sudo rm -rf "${BIN_INSTALL_PATH}/${BINARY_NAME}"
 fi
 echo "Successfully uninstalled ${BINARY_NAME}"
 
